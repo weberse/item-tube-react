c44bb6e512bb2fdbfafa9350d49ed11cd9d638a0
Den <dfitsak@gmail.com>
2015-03-31 16:23:46 +0200

    add play button states

--- src/tube/Tube.js
+++ src/tube/Tube.js
@@ -1,11 +1,24 @@
 
 var Tube = React.createClass( {
+
+    getInitialState: function() {
+        return {data: {
+            state: 'pending'
+        }};
+    },
+
+    componentDidMount: function() {
+        this.setState({data: {
+            state: 'stop'
+        }});
+    },
+
   	render: function() {
       	return (
       		<div>
       			<p>Tube</p>
       			<Title />
-      			<Play />
+      			<Play data={this.state.data} />
       		</div>
       	);
   	}
--- src/tube/components/play.js
+++ src/tube/components/play.js
@@ -1,9 +1,29 @@
+var states = {
+    play: 'Play',
+    pending: 'Loading...',
+    stop: 'Stop'
+};
 
 var Play = React.createClass({
-	
+
+    getInitialState: function(){
+
+        return {
+            playText: states[this.props.data.state]
+        };
+    },
+
+    componentWillReceiveProps: function(nextProps) {
+        this.setState({
+            playText: states[nextProps.data.state]
+        });
+    },
+
 	render: function() {
 		return (
-			<a>play</a>
+            <div>
+			    <a>{this.state.playText}</a>
+            </div>
 		);
 	}
 });
